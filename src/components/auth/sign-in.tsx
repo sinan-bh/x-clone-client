db07bf4a4e0770a3471988fe20418ec82b5210ec
sinan <sinanbh9605@gmail.com>
2024-12-12 16:55:08 +0530

    authintication ui completed

---
+++ src/components/auth/sign-in.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+type User = [
+  {
+    userName: string;
+    email: string;
+    password: string;
+  }
+];
+
+const SignIn = () => {
+  const router = useRouter();
+  const [loginUser, setLoginUser] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setError(null);
+
+    if (!loginUser || !password) {
+      setError("Please enter both username/email and password.");
+      return;
+    }
+
+    try {
+      const storedUser = localStorage.getItem("x-users");
+
+      if (!storedUser) {
+        setError("No user found. Please register first.");
+        return;
+      }
+
+      const users: User = JSON.parse(storedUser);
+      console.log(users);
+
+      const user = users.find(
+        (u) =>
+          (u.userName === loginUser || u.email === loginUser) &&
+          u.password === password
+      );
+
+      if (
+        (loginUser === user?.userName || loginUser === user?.email) &&
+        password === user?.password
+      ) {
+        alert("Sign In Successful");
+        router.push(`/home`);
+      } else {
+        setError("Invalid username/email or password.");
+      }
+    } catch (err) {
+      console.error("Error reading user data from localStorage:", err);
+      setError("An unexpected error occurred. Please try again.");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-800">
+      <div className="w-full max-w-md bg-black p-8 rounded-lg shadow-lg">
+        <h2 className="text-3xl font-semibold text-start text-white mb-6">
+          Sign in to X
+        </h2>
+        {error && <p className="text-red-500 text-center mb-4">{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="loginUser"
+              className="block text-gray-500 font-medium"
+            >
+              Username or Email
+            </label>
+            <input
+              type="text"
+              id="loginUser"
+              className="w-full p-3 mt-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-800"
+              placeholder="Enter your username or email"
+              value={loginUser}
+              onChange={(e) => {
+                setLoginUser(e.target.value);
+                setError(null);
+              }}
+            />
+          </div>
+
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-gray-500 font-medium"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full p-3 mt-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-800"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                setError(null);
+              }}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            Sign In
+          </button>
+        </form>
+        <p className="text-center mt-4 text-gray-500">
+          Don&apos;t have an account?{" "}
+          <a href="/signup" className="text-blue-600 hover:underline">
+            Sign up
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ src/components/auth/sign-up.tsx
@@ -0,0 +1,138 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const Register = () => {
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    userName: "",
+    password: "",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    profilePicture: "",
+  });
+
+  const [error, setError] = useState<string | null>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const { name, email, userName, password } = formData;
+
+    if (!name || !email || !userName || !password) {
+      setError("Please fill in all fields.");
+      return;
+    }
+
+    setError(null);
+    formData.profilePicture = formData.name.charAt(0).toUpperCase();
+    const users = JSON.parse(localStorage.getItem("x-users") || "[]");
+    users.push(formData);
+    localStorage.setItem("x-users", JSON.stringify(users));
+
+    console.log("Registration successful:", formData);
+    alert("Sign Up Completed");
+    router.push("/signin");
+  };
+
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-800">
+      <div className="w-full max-w-lg bg-black p-8 rounded-lg shadow-lg">
+        <h2 className="text-3xl font-semibold text-start text-white mb-6">
+          Sign up to X
+        </h2>
+        {error && <p className="text-red-500 text-center mb-4">{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-gray-500 font-medium">
+              Full Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full p-3 mt-2 text-black border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Enter your full name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-500 font-medium">
+              Email Address
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full p-3 mt-2 text-black border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Enter your email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="userName"
+              className="block text-gray-500 font-medium"
+            >
+              User Name
+            </label>
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              className="w-full p-3 mt-2 text-black border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-700"
+              placeholder="Enter your user name"
+              value={formData.userName}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-gray-500 font-medium"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full p-3 mt-2 text-black border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Enter your password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            Register
+          </button>
+        </form>
+        <p className="text-center mt-4 text-gray-600">
+          Already have an account?{" "}
+          <a href="/signin" className="text-blue-600 hover:underline">
+            Sign In
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
