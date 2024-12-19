15083ef6a1f6250d1bf0fb6da6f0e6370cb41918
sinan <sinanbh9605@gmail.com>
2024-12-19 16:59:41 +0530

    updated verify user

--- src/components/auth/verify-username.tsx
+++ src/components/auth/verify-username.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 export default function VerifyUserName() {
